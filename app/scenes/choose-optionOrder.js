63dc07b88d3f76dfd9e694052b1a81f036d8ced9
abdulazizv <abdulazizheir@gmail.com>
2023-05-08 14:24:16 +0500

    commited changes

---
+++ app/scenes/choose-optionOrder.js
@@ -0,0 +1,31 @@
+const Markup = require("telegraf/markup");
+const WizardScene = require("telegraf/scenes/wizard");
+const Composer = require("telegraf/composer");
+const { match } = require("telegraf-i18n");
+const { ChatsModel } = require("../models");
+const { saveSession } = require("../methods");
+
+function BasicCommandsHandler(handler) {
+    if (!handler) {
+      handler = new Composer();
+    }
+    handler.command(
+      "start",
+      async (ctx) => (await global.routes.start(ctx, false), ctx.scene.leave())
+    );
+    return handler;
+  
+}
+
+module.exports = new WizardScene(
+    'choose-option',
+        async (ctx) => {
+            ctx.replyWithHTML(
+                ctx.i18n.t("start-order"),
+                Markup.keyboard([
+                    [ctx.i18n.t("")]
+                ])
+            )
+        }
+    
+)
